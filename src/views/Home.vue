2fe3cfd495813f2708fbbff878387e34af648e72
Al-Ameen Fadipe <Al-Ameen.Fadipe@sterling.ng>
2022-06-18 12:06:59 +0100

    home page ui mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,27 +6,32 @@
       <!-- <div class=""> -->
       <div class="grid lg:grid-cols-2 sm:relative md:grid-cols-none pb-10 ">
         <div class="pt-10">
-          <div class="flex landing-text">
+          <div class="flex landing-text text-3xl lg:text-7xl">
             Find Healthcare <br> You Need, Anywhere You Are.
           </div>
-          <p class="clinicblue" style="font-size: 24px">
-            Find the best and most credible healthcare service providers near
-            you.
-          </p>
-          <div class="p-3">
+          <div class="pt-5 lg:pt-10">
+            <p class="clinicblue text-base lg:text-2xl">
+              Find the best and most credible healthcare service providers near
+              you.
+            </p>
+          </div>
+          <div class="p-3 pt-10">
             <router-link to="/direction">
               <button
                 class="
                   bg-transparent
                   font-bold
                   hover:text-white
-                  py-2
-                  px-4
+                  lg:py-2
+                  lg:px-4
                   landing-page-button1
                   hover:border-transparent
                   rounded-full
+                  h-8
+                  lg:h-11
+                  w-2/5
+                  lg:w-56
                 "
-                style="width: 223px"
               >
                 Get Directions
               </button>
@@ -38,11 +43,14 @@
                   landing-page-button2
                   text-white
                   font-bold
-                  py-2
-                  px-4
+                  lg:py-2
+                  lg:px-4
+                  h-8
+                  lg:h-11
                   rounded-full
+                  w-2/5
+                  lg:w-56
                 "
-                style="width: 223px"
               >
                 Leave a Review
               </button>
@@ -55,10 +63,12 @@
                 bg-transparent
                 font-bold
                 hover:text-white
-                py-2
+                lg:py-2
                 px-4
                 hover:border-transparent
                 rounded-full
+                h-8
+                lg:h-11
                 landing-page-button3
                 w-4/5
               "
@@ -68,7 +78,7 @@
           </router-link>
           
         </div>
-        <div class="pt-10 lg:inline-block">
+        <div class="pt-10 hidden lg:inline-block">
           <img src="../assets/landing-image.svg" alt="landing-image" />
         </div>
       </div>
@@ -77,23 +87,23 @@
     <section class="justify-center md:mx-auto max-w-7xl px-4">
       <div class="grid bg-blue justify-center lg:grid-cols-2 sm:grid-cols-1">
         <div class="lg:place-self-end mt-10">
-          <div class="mapouter lg:w-max">
+          <div class="lg:mapouter lg:w-max">
 
             <iframe class="lg:block hidden" src="https://quizzical-leakey-6ce101.netlify.app/" width="566px" height="620px" frameborder="0">
             </iframe>
 
-            <iframe class="lg:hidden md:inline" src="https://quizzical-leakey-6ce101.netlify.app/" width="100%" height="620px" frameborder="0">
+            <iframe class="lg:hidden md:inline" src="https://quizzical-leakey-6ce101.netlify.app/" width="329px" height="175px" frameborder="0">
             </iframe>
 
             <!-- <div style="width:100%;height:630px;">
                 <locator/>
             </div> -->
             
-        </div>
+          </div>
         </div>
         <div class="lg:content-start lg:ml-2 mt-10">
           
-          <div class="address-card">
+          <div class="address-card hidden lg:inline-block">
             <div class="">
               <h3 class="clinicblue text-center" style="font-weight: 900; font-size: 35px;">Top Picks For You:</h3>
               <hr class="mb-6 m-auto" style="width: 90%;">
@@ -138,6 +148,34 @@
               </div>
               <hr class="mt-4 w m-auto" style="width: 90%;">
               <p class="clinicblue text-center" style="font-weight: 900; font-size: 28px;">More Options</p>
+            </div>
+          </div>
+
+          <div class="address-card lg:hidden">
+            <div class="">
+              <h3 class="clinicblue text-center" style="font-weight: 800; font-size: 14px;">Top Picks For You:</h3>
+              <hr class="mb-6 m-auto" style="width: 90%;">
+              <div class="grid bg-blue grid-cols-4 ">
+                <div class="text-center col-span-3 ">
+                  <h4 class="text-white text-sm font-bold text-center">ClearEye Hospital</h4>
+                  <p class="text-white text-sm text-center">41, Ago Palace Way, Lagos</p>
+                </div>
+                <div>
+                  <img class="pl-10 pt-5 justify-self-end" src="../assets/small_arrow_right.png" alt="arrow">
+                </div>
+              </div>
+              <hr class="my-6 m-auto" style="width: 90%;">
+              <div class="grid bg-blue grid-cols-4 ">
+                <div class="text-center col-span-3 ">
+                  <h4 class="text-white text-sm font-bold">Way Maker Clinic</h4>
+                  <p class="text-white text-sm">411, Akoka-Yaba, Lagos.</p>
+                </div>
+                <div>
+                  <img class="pl-10 pt-5 justify-self-end" src="../assets/small_arrow_right.png" alt="arrow">
+                </div>
+              </div>
+              <hr class="mt-4 w m-auto" style="width: 90%;">
+              <p class="clinicblue text-center" style="font-weight: 800; font-size: 14px;">More Options</p>
             </div>
           </div>
           
